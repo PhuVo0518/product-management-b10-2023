976f8034d650654d1be001140bf2fb584823399d
Phu Vo <u1153426@umail.usq.edu.au>
2023-12-29 12:56:57 +1000

    Configure UI of Product List page (Admin)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,13 @@
+const Product = require("../../models/product.model");
+
 // [GET] /admin/products
-module.exports.index = (request, response) => {
+module.exports.index = async (request, response) => {
+    const products = await Product.find({
+        deleted: false,
+    });
+
     response.render("admin/pages/products/index.pug", {
         pageTitle: "Product List",
+        products: products,
     });
 };
