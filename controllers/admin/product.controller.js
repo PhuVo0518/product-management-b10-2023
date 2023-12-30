bd632a76109efd57036eb06dcd542c6b6966f005
Phu Vo <u1153426@umail.usq.edu.au>
2023-12-30 11:09:23 +1000

    Implement feature: Filter by Status

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,12 +2,46 @@ const Product = require("../../models/product.model");
 
 // [GET] /admin/products
 module.exports.index = async (request, response) => {
-    const products = await Product.find({
+    const filterState = [
+        {
+            name: "All",
+            status: "",
+            class: "",
+        },
+        {
+            name: "Active",
+            status: "active",
+            class: "",
+        },
+        {
+            name: "Inactive",
+            status: "inactive",
+            class: "",
+        },
+    ];
+
+    if (request.query.status) {
+        const index = filterState.findIndex(
+            (item) => (item.status = request.query.status)
+        );
+        filterState[index].class = "active";
+    } else {
+        filterState[0].class = "active";
+    }
+
+    const find = {
         deleted: false,
-    });
+    };
+
+    if (request.query.status) {
+        find.status = request.query.status;
+    }
+
+    const products = await Product.find(find);
 
     response.render("admin/pages/products/index.pug", {
         pageTitle: "Product List",
         products: products,
+        filterState: filterState,
     });
 };
