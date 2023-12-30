f61914be0a85d65ac9d44e68519d28df060a5a35
Phu Vo <u1153426@umail.usq.edu.au>
2023-12-30 13:01:08 +1000

    Implement feature: Search

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,6 +2,7 @@ const Product = require("../../models/product.model");
 
 // [GET] /admin/products
 module.exports.index = async (request, response) => {
+    // Feature: Filter by State
     const filterState = [
         {
             name: "All",
@@ -29,6 +30,8 @@ module.exports.index = async (request, response) => {
         filterState[0].class = "active";
     }
 
+    // End Feature: Filter by State
+
     const find = {
         deleted: false,
     };
@@ -37,11 +40,19 @@ module.exports.index = async (request, response) => {
         find.status = request.query.status;
     }
 
+    // Feature: Search
+    if (request.query.keyword) {
+        const regex = new RegExp(request.query.keyword, "i");
+        find.title = regex;
+    }
+    // End Feature: Search
+
     const products = await Product.find(find);
 
     response.render("admin/pages/products/index.pug", {
         pageTitle: "Product List",
         products: products,
         filterState: filterState,
+        keyword: request.query.keyword,
     });
 };
