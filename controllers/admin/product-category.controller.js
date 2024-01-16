31f7da2dac0b4619a1f7e14b8e8b8e0eab837959
Phu Vo <u1153426@umail.usq.edu.au>
2024-01-16 20:24:13 +1000

    Implement Product Category Admin (add a new category)

---
+++ controllers/admin/product-category.controller.js
@@ -0,0 +1,33 @@
+const ProductCategory = require("../../models/product-category.model");
+const systemConfig = require("../../config/system");
+
+// [GET] /admin/products-category
+module.exports.index = (request, response) => {
+  response.render("admin/pages/products-category/index.pug", {
+    pageTitle: "Product Category",
+  });
+};
+
+// [GET] /admin/products-category/create
+module.exports.create = (request, response) => {
+  response.render("admin/pages/products-category/create.pug", {
+    pageTitle: "Add a new product category",
+  });
+};
+
+// [POST] /admin/products-category/createPost
+module.exports.createPost = async (request, response) => {
+  if (request.body.position == "") {
+    const countRecords = await ProductCategory.countDocuments();
+    request.body.position = countRecords + 1;
+  } else {
+    request.body.position = parseInt(request.body.position);
+  }
+
+  const record = new ProductCategory(request.body);
+  await record.save();
+
+  request.flash("success", "Add a new product category successfully!");
+
+  response.redirect(`/${systemConfig.prefixAdmin}/products-category`);
+};
