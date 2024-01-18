1d78cfa83e498633ca1d2644dfe5354c30a93fcb
Phu Vo <u1153426@umail.usq.edu.au>
2024-01-18 11:06:41 +1000

    Feature: Add a new role (Admin)

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,7 +1,32 @@
-// [GET] /admin/roles/
+const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 
-module.exports.index = (request, response) => {
+// [GET] /admin/roles/
+module.exports.index = async (request, response) => {
+  const records = await Role.find({
+    deleted: false,
+  });
+
   response.render("admin/pages/roles/index.pug", {
     pageTitle: "Roles",
+    records: records,
   });
+};
+
+// [GET] /admin/roles/create
+module.exports.create = (request, response) => {
+  response.render("admin/pages/roles/create.pug", {
+    pageTitle: "Add a new role",
+  });
+};
+
+// [POST] /admin/roles/create
+module.exports.createPost = async (request, response) => {
+  const record = new Role(request.body);
+
+  await record.save();
+
+  request.flash("success", "Add a new role successfully!");
+
+  response.redirect(`/${systemConfig.prefixAdmin}/roles`);
 };
